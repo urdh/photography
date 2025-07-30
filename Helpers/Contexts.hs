23040944d4d7bc3c506ff35bece82942ea1974ee
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2025-07-30 21:26:41 +0200

    Use most recent change for `updated` date
    With this change, `updated` for collections will contain the most
    recent date of `published`, `updated`, and `taken` of each image.

--- Helpers/Contexts.hs
+++ Helpers/Contexts.hs
@@ -5,18 +5,23 @@ module Helpers.Contexts
   ) where
 
 import           Control.Applicative            (Alternative (..))
+import           Control.Conditional            (if')
+import           Control.Monad                  (ap, liftM2, msum, (<=<))
 import           Data.Binary                    (Binary (..))
-import           Data.List                      (uncons)
-import           Data.Time.Format               (defaultTimeLocale, formatTime)
+import           Data.List                      (singleton)
+import           Data.Time.Clock                (UTCTime)
+import           Data.Time.Format               (TimeLocale, defaultTimeLocale, formatTime,
+                                                 parseTimeM)
 import           Hakyll.Core.Compiler           (Compiler, getRoute, loadAll,
                                                  loadAllSnapshots)
-import           Hakyll.Core.Identifier         (setVersion, toFilePath)
+import           Hakyll.Core.Identifier         (Identifier, setVersion, toFilePath)
 import           Hakyll.Core.Identifier.Pattern (Pattern, fromRegex, fromGlob,
                                                  hasNoVersion, (.&&.))
 import           Hakyll.Core.Item               (Item (..))
+import           Hakyll.Core.Metadata           (MonadMetadata (..), lookupString)
 import           Hakyll.Web.Html                (toUrl)
 import           Hakyll.Web.Template.Context    (Context (..), defaultContext,
-                                                 field, functionField,
+                                                 field, functionField, getItemUTC,
                                                  listField, listFieldWith)
 import           Helpers.Photos                 (alphabetical, chronological,
                                                  exifKeyField, getPhotoItemDate,
@@ -40,20 +45,25 @@ photoContext rootContext indexPaths extensions =
   rootContext                        <>
   defaultContext
   where
-    collectionItems = ((.&&.) indexPaths) . fromGlob . (\p -> takeDirectory p </> "*") . toFilePath
+    collectionItems = (.&&.) indexPaths . fromGlob . (\p -> takeDirectory p </> "*") . toFilePath
     collections = flip loadAllSnapshots "raw" . collectionItems . itemIdentifier
 
 collectionContext :: Context String -> Pattern -> String -> Context String
 collectionContext rootContext indexPaths extensions =
-  field "updated"
-    (fmap (maybe empty (photoDate "%Y-%m-%d" . fst) . uncons) . photos)          <>
-  listFieldWith "photos" (photoContext rootContext indexPaths extensions) photos <>
-  rootContext                                                                    <>
+  field         "updated" (fmap formatDate . maximum' <=< getAllDates)            <>
+  listFieldWith "photos"  (photoContext rootContext indexPaths extensions) photos <>
+  rootContext                                                                     <>
   defaultContext
   where
     collectionTiffs = fromRegex . (\p -> takeDirectory p </> extensions) . toFilePath
     photos = fmap (reverse . chronological) . (photosFrom . collectionTiffs . itemIdentifier)
-    photoDate fmt = formatTime defaultTimeLocale fmt . getPhotoItemDate . itemIdentifier
+    formatDate = formatTime defaultTimeLocale "%Y-%m-%d"
+    getPhotoDates = map (getPhotoItemDate . itemIdentifier)
+    getUpdateDate = flip (<|>) (return []) . fmap singleton . getUpdateUTC defaultTimeLocale . itemIdentifier
+    getPublishDate = flip (<|>) (return []) . fmap singleton . getItemUTC defaultTimeLocale  . itemIdentifier
+    getAllDates = concatenate' getUpdateDate  getPublishDate (fmap getPhotoDates . photos)
+    concatenate' = (. liftM2 (liftA2 (++))) . (.) . liftM2 (liftA2 (++))
+    maximum' = ap (flip if' empty . null) (return . maximum)
 
 archiveContext :: Context String -> Pattern -> Pattern -> String -> Context String
 archiveContext rootContext indexPaths photoPaths extensions =
@@ -68,3 +78,24 @@ archiveContext rootContext indexPaths photoPaths extensions =
 --------------------------------------------------------------------------------
 photosFrom :: (Binary a, Typeable a) => Pattern -> Compiler [Item a]
 photosFrom p = loadAll (p .&&. hasNoVersion)
+
+getUpdateUTC :: (Alternative m, MonadMetadata m) => TimeLocale -> Identifier -> m UTCTime
+getUpdateUTC locale id' = do
+    metadata <- getMetadata id'
+    let tryField k fmt = lookupString k metadata >>= parseTime' fmt
+    maybe empty return $ msum [tryField "updated" fmt | fmt <- formats]
+  where
+    parseTime' = parseTimeM True locale
+    formats    =
+        [ "%a, %d %b %Y %H:%M:%S %Z"
+        , "%a, %d %b %Y %H:%M:%S"
+        , "%Y-%m-%dT%H:%M:%S%Z"
+        , "%Y-%m-%dT%H:%M:%S"
+        , "%Y-%m-%d %H:%M:%S%Z"
+        , "%Y-%m-%d %H:%M:%S"
+        , "%Y-%m-%d"
+        , "%d.%m.%Y"
+        , "%B %e, %Y %l:%M %p"
+        , "%B %e, %Y"
+        , "%b %d, %Y"
+        ]
