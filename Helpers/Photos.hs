d4fd8a7b9b2343b35d671cef7219443a94239af0
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2026-01-22 20:22:42 +0100

    Add support for metadata as exported by ON1 Photo RAW 2026
    Straight-forward exports from ON1 contain most of the necessary metadata
    (except the creative commons license), but most of it is exported as XMP
    rather than plain IPTC EXIF tags. This means we need to be able to fall
    back to XMP in cases where the EXIF tags are missing.
    
    Additionally, TIFF files exported by ON1 are subtly broken and won't be
    read correctly by the JuicyPixels library backing hakyll-images. Until
    this issue is fixed, we need to export JPEG versions which means we also
    have to parse the *additional* APP1 segment containing the XMP data (the
    EXIF application notes key which normally contains XMP will be empty in
    these cases). This essentially forces us to implement a trimmed copy of
    the juicy-pixels JPEG parser.

--- Helpers/Photos.hs
+++ Helpers/Photos.hs
@@ -11,26 +11,26 @@ module Helpers.Photos
   , chronological
   ) where
 
-import           Control.Applicative         (Alternative (..))
+import           Control.Applicative         (Alternative (..), (<|>), asum)
+import           Control.Monad               (ap)
 import qualified Data.ByteString             as DBS
 import qualified Data.ByteString.Char8       as DBC
-import           Data.Text.Normalize
-import           Data.Either                 (fromRight)
+import qualified Data.ByteString.Lazy        as DBL
 import           Data.List                   (sortOn)
-import           Data.List.Extra             (headDef)
 import qualified Data.Map.Strict             as DMS
-import           Data.Maybe                  (catMaybes, fromJust)
+import           Data.Maybe                  (fromJust, fromMaybe, listToMaybe)
 import qualified Data.Text                   as T
-import           Data.Text.Encoding          (decodeUtf8, decodeASCII)
+import           Data.Text.Encoding          (decodeUtf8)
+import           Data.Text.Normalize
 import           Data.Time.Clock             (UTCTime (..))
-import           Data.Time.Format            (defaultTimeLocale, formatTime,
-                                              parseTimeM)
+import           Data.Time.Format            (defaultTimeLocale, formatTime, parseTimeM)
 import           Graphics.HsExif
 import           Hakyll.Core.Identifier      (Identifier (..), toFilePath)
 import           Hakyll.Core.Item            (Item (..))
-import           Hakyll.Web.Template.Context (Context (..), field,
-                                              functionField)
+import           Hakyll.Web.Template.Context (Context (..), field, functionField)
+import           Helpers.Metadata            (Metadata (..), readMetadata)
 import           System.FilePath             (takeBaseName)
+import           System.IO                   (IOMode (..), withFile)
 import           System.IO.Unsafe            (unsafePerformIO)
 import           Text.RE.TDFA.String
 import           Text.XML.HXT.Core
@@ -72,7 +72,7 @@ photoExifField key =
        if null v
          then empty
          else return v) $
-    (getExifValue . head $ k) $ getExifMap i
+    (getMetadataValue . head $ k) $ getMetadata i
 
 exifKeyField :: String -> String -> Context a
 exifKeyField key k =
@@ -81,7 +81,7 @@ exifKeyField key k =
        if null v
          then empty
          else return v) $
-    getExifValue k $ getExifMap i
+    getMetadataValue k $ getMetadata i
 
 chronological :: [Item a] -> [Item a]
 chronological = sortOn $ getPhotoItemDate . itemIdentifier
@@ -95,66 +95,82 @@ splitPhotoFilename str =
     matches' xs = error $ unwords ["Invalid photo filename:", show xs]
 
 --------------------------------------------------------------------------------
-getExifMap :: Item a -> DMS.Map ExifTag ExifValue
-getExifMap = fromRight DMS.empty . unsafePerformIO . parseFileExif . toFilePath . itemIdentifier
+getMetadata :: Item a -> [Metadata] -- TODO: This re-opens the file?
+getMetadata = unsafePerformIO . parseFileMetadata . toFilePath . itemIdentifier
+  where
+    parseFileMetadata :: FilePath -> IO [Metadata]
+    parseFileMetadata = flip (`withFile` ReadMode) (fmap (readMetadata . DBL.fromStrict) . DBS.hGetContents)
 
-getExifValue :: String -> DMS.Map ExifTag ExifValue -> String
+getMetadataValue :: String -> [Metadata] -> String
+getMetadataValue = ((fromMaybe "" . asum) .) . map . getValue
+  where
+    getValue :: String -> Metadata -> Maybe String
+    getValue k (ExifData e) = getExifValue k e
+    getValue k (XmpData e) = getXmpValue k e
+
+getXmpValue :: String -> T.Text -> Maybe String
+getXmpValue = (. T.unpack) . getValue
+  where
+    getValue :: String -> (String -> Maybe String)
+    getValue "artist"    = listToMaybe . findXmpValue "dc:creator"
+    getValue "copyright" = listToMaybe . findXmpValue "dc:rights"
+    getValue "license"   = listToMaybe . findXmpValue "cc:license"
+    getValue "title"     = listToMaybe . findXmpValue "dc:description"
+    getValue _           = const Nothing
+    -- Get all values of the given attribute from the input XML (TODO: too lenient!)
+    findXmpValue :: String -> String -> [String]
+    findXmpValue key = runLA (xreadDoc >>> multi (
+                           (isElem >>> getAttrValue0 key) <+>
+                           (isElem >>> hasName key >>> getAttrValue0 "rdf:resource") <+>
+                           (isElem >>> hasName key /> (hasName "rdf:Seq" <+> hasName "rdf:Alt")
+                             /> hasName "rdf:li" /> isText >>> getText)
+                       ))
+
+getExifValue :: String -> DMS.Map ExifTag ExifValue -> Maybe String
 getExifValue = getValue
   where
     -- Finding EXIF values in the EXIF key-value map
-    findAll :: [ExifTag] -> DMS.Map ExifTag ExifValue -> [(ExifTag, ExifValue)]
-    findAll = (catMaybes .) . flip (map . flip findOne)
-    findOne :: ExifTag -> DMS.Map ExifTag ExifValue -> Maybe (ExifTag, ExifValue)
-    findOne key' = withKey . DMS.lookup key'
+    findKeys :: [ExifTag] -> DMS.Map ExifTag ExifValue -> Maybe [(ExifTag, ExifValue)]
+    findKeys = (sequence .) . flip (map . flip findKey)
+    findKey :: ExifTag -> DMS.Map ExifTag ExifValue -> Maybe (ExifTag, ExifValue)
+    findKey key' = withKey . DMS.lookup key'
       where
         withKey (Just value) = Just (key', value)
         withKey _            = Nothing
     -- Maps string keys to specific (potentially composed) EXIF tags
     -- See http://hackage.haskell.org/package/hsexif-0.6.1.6/docs/Graphics-HsExif.html for keys
-    getValue :: String -> DMS.Map ExifTag ExifValue -> String
-    getValue "artist"   = unwords . map (prettify fixEncoding)                      . findAll [artist]
-    getValue "camera"   = unwords . map (prettify fixEncoding)                      . findAll [make, model]
-    getValue "lens"     = unwords . unclutter . map (prettify fixEncoding)          . findAll [make, lensMake, lensModel]
-    getValue "filmtype" = unwords . map (prettify id)                               . findAll [userComment]
-    getValue "shutter"  = unwords . take 1 . map (prettify (T.replace " sec." "s")) . findAll [exposureTime, shutterSpeedValue]
-    getValue "aperture" = unwords . take 1 . map (prettify (T.replace "f/" "ƒ/"))   . findAll [fnumber, apertureValue]
-    getValue "speed"    = unwords . map (prettify id)                               . findAll [isoSpeedRatings]
-    getValue "copyright"= unwords . map (prettify fixEncoding)                      . findAll [copyright]
-    getValue "license"  = headDef "" . concatMap (getXmlAttribute' "cc:license")    . findAll [applNotes]
-    getValue "location" = const "" -- TODO: read EXIF tag and do GPS lookup
-    getValue "title"    = unwords . map (prettify fixEncoding)                      . findAll [imageDescription]
-    getValue tag        = error $ unwords ["Unknown EXIF field:", tag]
+    getValue :: String -> (DMS.Map ExifTag ExifValue -> Maybe String)
+    getValue "artist"    = fmap (prettify fixEncoding) . findKey artist
+    getValue "camera"    = ((asum .) . flip (map . flip id)) $
+                            map (fmap (unwords . map (prettify fixEncoding)) .)
+                            [findKeys [make, model], findKeys [cameraMake, cameraModel]]
+    getValue "lens"      = (fmap (unwords . map (prettify fixEncoding)) .) =<<
+                            (. findKeys [lensMake, lensModel]) . fmap . unclutter .
+                            ap ((<|>) . findKey make) (findKey cameraMake)
+    getValue "filmtype"  = fmap (prettify id) . findKey userComment
+    getValue "shutter"   = ((asum .) . flip (map . flip id)) $
+                            map (fmap (prettify (T.replace " sec." "s")) .)
+                            [findKey exposureTime, findKey shutterSpeedValue]
+    getValue "aperture"  = ((asum .) . flip (map . flip id)) $
+                            map (fmap (prettify (T.replace "f/" "ƒ/")) .)
+                            [findKey fnumber, findKey apertureValue]
+    getValue "speed"     = fmap (prettify id) . findKey isoSpeedRatings
+    getValue "copyright" = fmap (prettify fixEncoding) . findKey copyright
+    getValue "location"  = const Nothing -- TODO: read EXIF tag and do GPS lookup
+    getValue "title"     = fmap (prettify fixEncoding) . findKey imageDescription
+    getValue _           = const Nothing
     -- Pretty-print an ExifValue given the ExifTag it belongs to
     prettify :: (T.Text -> T.Text) -> (ExifTag, ExifValue) -> String
     prettify f = T.unpack . f . uncurry prettyPrinter
     -- Fix encoding of string values by reinterpreting the string as UTF-8
     fixEncoding :: T.Text -> T.Text
     fixEncoding = normalize NFC . decodeUtf8 . DBC.pack . T.unpack
-    -- Interpret the given EXIF value as UTF-8 text
-    asUtf8Text :: ExifValue -> Maybe T.Text
-    asUtf8Text (ExifText       v) = Just . fixEncoding . T.pack $ v
-    asUtf8Text (ExifNumberList v) = Just . decodeUtf8 . DBS.pack . map fromIntegral $ v
-    asUtf8Text (ExifUndefined  v) = uncurry decodeExifUndefined . DBS.splitAt 8 $ v
-    asUtf8Text _ = Nothing
-    -- Interpret an ExifUndefined value given its prefix
-    decodeExifUndefined :: DBC.ByteString -> DBC.ByteString -> Maybe T.Text
-    decodeExifUndefined "ASCII\0\0\0" v = Just . decodeASCII $ v
-    decodeExifUndefined "UNICODE\0"   v = Just . decodeUtf8 $ v
-    decodeExifUndefined _ _ = Nothing
-    -- Wrap getXmlAttribute to work with ExifValue items
-    getXmlAttribute' :: String -> (ExifTag, ExifValue) -> [String]
-    getXmlAttribute' key = maybe [] (getXmlAttribute key . T.unpack) . asUtf8Text . snd
-    -- Get all values of the given attribute from the input XML
-    getXmlAttribute :: String -> String -> [String]
-    getXmlAttribute key = runLA (xreadDoc >>> multi (
-                              (isElem >>> getAttrValue0 key) <+>
-                              (isElem >>> hasName key >>> getAttrValue0 "rdf:resource")
-                          ))
     -- Unclutter the lens make by dropping is if it is the same as the camera make
-    unclutter :: Eq a => [a] -> [a]
-    unclutter (x:y:xs) = [y | x /= y] ++ xs
-    unclutter xs = xs
+    unclutter :: Maybe (ExifTag, ExifValue) -> [(ExifTag, ExifValue)] -> [(ExifTag, ExifValue)]
+    unclutter (Just (_, x)) (y:xs) = [y | x /= snd y] ++ xs
+    unclutter _ xs = xs
     -- Keys not in hsexif
-    applNotes = ExifTag IFD0 (Just "applicationNotes") 0x02bc $ T.pack . show
-    lensMake  = ExifTag ExifSubIFD (Just "lensMake")  0xa433 $ T.pack . show
-    lensModel = ExifTag ExifSubIFD (Just "lensModel") 0xa434 $ T.pack . show
+    lensMake    = ExifTag ExifSubIFD (Just "lensMake")    0xa433 $ T.pack . show
+    lensModel   = ExifTag ExifSubIFD (Just "lensModel")   0xa434 $ T.pack . show
+    cameraMake  = ExifTag ExifSubIFD (Just "cameraMake")  0x010f $ T.pack . show
+    cameraModel = ExifTag ExifSubIFD (Just "cameraModel") 0x0110 $ T.pack . show
